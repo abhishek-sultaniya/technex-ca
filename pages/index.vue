f689a494b1bfce1a266bc9c144bcd8f120c3bc44
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2022-12-16 12:41:24 +0530

    Add useFetch

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>{{ data }}</div>
+</template>
+
+<script setup>
+const { data } = await useFetch('/ca/register', {
+  method: 'post',
+  baseURL: 'https://api.technex.co.in/',
+  body: {
+    year: null,
+    email: 'string',
+    mobile_number: 'string',
+    whyChooseYou: 'string',
+    whatsapp_number: 'string',
+    college: 'string',
+    postal_address: 'string',
+    pinCode: 2147483647,
+    password: 'string',
+    username: 'string',
+    last_name: 'string',
+    first_name: 'string',
+    wereCa: true,
+  },
+})
+</script>
